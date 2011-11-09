a67554f53278cb30a5e58c89e8594340d904b233
Markus Tacker <m@tacker.org>
2011-11-09 17:22:10 +0100

    Beginne mit Nummern raten.

---
+++ Uebung_03-Aufgabe_01/src/de/hsrm/medieninf/mobcomp/ueb03/aufg01/Guess.java
@@ -0,0 +1,45 @@
+package de.hsrm.medieninf.mobcomp.ueb03.aufg01;
+
+import java.math.BigInteger;
+
+import android.util.Log;
+
+public class Guess {
+	private BigInteger number;
+	private int result; 
+	
+	public Guess(BigInteger number, int result)
+	{
+		this.setNumber(number);
+		this.setResult(result);
+		Log.v("Guess", "" + result);
+	}
+
+	public BigInteger getNumber() {
+		return number;
+	}
+
+	public void setNumber(BigInteger number) {
+		this.number = number;
+	}
+
+	public int getResult() {
+		return result;
+	}
+
+	public void setResult(int result) {
+		this.result = result;
+	}
+
+	public boolean isTooLow() {
+		return result < 0;
+	}
+	
+	public boolean isTooHigh() {
+		return result > 0;
+	}
+	
+	public boolean isGood() {
+		return !isTooHigh() && !isTooLow();
+	}
+}
