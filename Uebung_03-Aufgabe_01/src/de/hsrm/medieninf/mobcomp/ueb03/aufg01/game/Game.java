3411f71593fe7ad12cccb748a5fcb3cf8d9da37c
Markus Tacker <m@tacker.org>
2011-11-16 16:43:03 +0100

    Seeker dazu.

--- Uebung_03-Aufgabe_01/src/de/hsrm/medieninf/mobcomp/ueb03/aufg01/game/Game.java
+++ Uebung_03-Aufgabe_01/src/de/hsrm/medieninf/mobcomp/ueb03/aufg01/game/Game.java
@@ -4,15 +4,19 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.util.Log;
 import de.hsrm.medieninf.mobcomp.ueb03.aufg01.entity.Guess;
 
 public class Game {
 	private List<Guess> guesses;
 	private BigInteger askNumber;
 	private BigInteger limit;
+	private BigInteger maxHint;
+	private BigInteger minHint;
 
 	public Game(long limit) {
-		this.limit = BigInteger.valueOf((long) limit);
+		this.limit = this.maxHint = BigInteger.valueOf((long) limit);
+		this.minHint = BigInteger.ONE; 
 		setGuesses(new ArrayList<Guess>());
 		setAskNumber(BigInteger.valueOf((long) (Math.random() * 1000)));
 	}
@@ -34,9 +38,14 @@ public class Game {
 	}
 
 	public Guess createGuess(BigInteger userNumber) {
-
 		Guess guess = new Guess(userNumber, userNumber.compareTo(askNumber));
 		guesses.add(guess);
+		if (guess.isTooHigh()) {
+			maxHint = userNumber;
+		}
+		if (guess.isTooLow()) {
+			minHint = userNumber;
+		}
 		return guess;
 	}
 
@@ -46,5 +55,13 @@ public class Game {
 
 	public Integer getTries() {
 		return guesses.size();
+	}
+
+	public BigInteger getMaxHint() {
+		return maxHint;
+	}
+	
+	public BigInteger getMinHint() {
+		return minHint;
 	}
 }
