7e4e46907fc4bb9d063b9bb842c911d9e731a894
mtack001 <mtack001@765090bb-25cc-441a-8e70-586dd888a160>
2011-11-28 13:27:10 +0000

    Übung 2, Aufgabe 4 dazu (Währungsrechner).
    git-svn-id: https://scm.mi.hs-rm.de/svn/2011mobcomp/2011mobcompmtack001/trunk@36 765090bb-25cc-441a-8e70-586dd888a160

---
+++ Uebung_02-Aufgabe_04/src/de/hsrm/medieninf/mobcomp/ueb02/aufg03/WaehrungsUmrechnerActivity.java
@@ -0,0 +1,209 @@
+package de.hsrm.medieninf.mobcomp.ueb02.aufg03;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class WaehrungsUmrechnerActivity extends Activity {
+	public static final String TAG = "WaehrungsUmrechnerActivity";
+	public static final int DIALOG_CURRENCY = 1;
+
+	private Waehrung waehrungA, waehrungB;
+	private EditText valueA, valueB;
+	private Button buttonA, buttonB;
+	private InputWatcher watcherA, watcherB;
+	private CurrencyField fieldA, fieldB;
+	private Map<Integer, Waehrung> waehrungen = new HashMap<Integer, Waehrung>();
+	private Button clickedButton;
+
+	private class CurrencyField {
+		private EditText field;
+		private Waehrung currency;
+		private String name;
+
+		public CurrencyField(String name, EditText field) {
+			setField(field);
+			setName(name);
+		}
+
+		public EditText getField() {
+			return field;
+		}
+
+		public void setField(EditText field) {
+			this.field = field;
+		}
+
+		public Waehrung getCurrency() {
+			return currency;
+		}
+
+		public void setCurrency(Waehrung currency) {
+			this.currency = currency;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+	}
+
+	private class InputWatcher implements TextWatcher {
+		private CurrencyField watch, update;
+		private InputWatcher updateWatcher;
+
+		public InputWatcher(CurrencyField watch, CurrencyField update) {
+			this.watch = watch;
+			this.update = update;
+		}
+
+		@Override
+		public void afterTextChanged(Editable s) {
+			Log.v(TAG, "UPDATE: " + update.getName());
+			update.getField().removeTextChangedListener(updateWatcher);
+			try {
+				update.getField().setText(
+						formatCurrency(update.getCurrency().asCurrency(
+								watch.getCurrency().asEuro(
+										Double.parseDouble(watch.getField()
+												.getText().toString())))));
+			} catch (NumberFormatException e) {
+				update.getField().setText("");
+			}
+			update.getField().addTextChangedListener(updateWatcher);
+		}
+
+		private String formatCurrency(BigDecimal d) {
+			return String.format("%.02f", d);
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+		}
+
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+		}
+
+		public void setUpdaterListener(InputWatcher updateWatcher) {
+			this.updateWatcher = updateWatcher;
+		}
+	}
+
+	private class SelectCurrencyListener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			clickedButton = (Button) v;
+			showDialog(DIALOG_CURRENCY);
+		}
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Waehrung euro = new Waehrung().setSymbol("€")
+				.setName((String) getResources().getString(R.string.conc_eur))
+				.setRate(1.0);
+		Waehrung usd = new Waehrung().setSymbol("$")
+				.setName((String) getResources().getString(R.string.conc_usd))
+				.setRate(0.7183392);
+		Waehrung grd = new Waehrung().setSymbol("₯")
+				.setName((String) getResources().getString(R.string.conc_grd))
+				.setRate(2.93470286e-6);
+		waehrungen.put(0, euro);
+		waehrungen.put(1, usd);
+		waehrungen.put(2, grd);
+
+		valueA = (EditText) findViewById(R.id.inputA);
+		valueB = (EditText) findViewById(R.id.inputB);
+		fieldA = new CurrencyField("A", valueA);
+		fieldB = new CurrencyField("B", valueB);
+		watcherA = new InputWatcher(fieldA, fieldB);
+		watcherB = new InputWatcher(fieldB, fieldA);
+		watcherA.setUpdaterListener(watcherB);
+		watcherB.setUpdaterListener(watcherA);
+		valueA.addTextChangedListener(watcherA);
+		valueB.addTextChangedListener(watcherB);
+		buttonA = (Button) findViewById(R.id.buttonA);
+		buttonB = (Button) findViewById(R.id.buttonB);
+		SelectCurrencyListener scl = new SelectCurrencyListener();
+		buttonA.setOnClickListener(scl);
+		buttonB.setOnClickListener(scl);
+
+		setWaehrungA(euro);
+		setWaehrungB(usd);
+	}
+
+	public Waehrung getWaehrungA() {
+		return waehrungA;
+	}
+
+	public void setWaehrungA(Waehrung waehrungA) {
+		this.waehrungA = waehrungA;
+		buttonA.setText(getWaehrungA().getSymbol());
+		fieldA.setCurrency(this.waehrungA);
+	}
+
+	public Waehrung getWaehrungB() {
+		return waehrungB;
+	}
+
+	public void setWaehrungB(Waehrung waehrungB) {
+		this.waehrungB = waehrungB;
+		buttonB.setText(getWaehrungB().getSymbol());
+		fieldB.setCurrency(this.waehrungB);
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case DIALOG_CURRENCY:
+			ArrayList<CharSequence> waehrungenNames = new ArrayList<CharSequence>();
+			for (Waehrung w : waehrungen.values()) {
+				waehrungenNames.add(w.getName());
+			}
+
+			return new AlertDialog.Builder(WaehrungsUmrechnerActivity.this)
+					.setTitle(R.string.currency_select_dialog_title)
+					.setItems(
+							waehrungenNames.toArray(new CharSequence[waehrungenNames
+									.size()]),
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int which) {
+									if(clickedButton.equals(buttonA)) {
+										setWaehrungA(waehrungen.get(which));
+										watcherA.afterTextChanged(fieldA.getField().getEditableText());
+									} else {
+										setWaehrungB(waehrungen.get(which));
+										watcherB.afterTextChanged(fieldB.getField().getEditableText());
+									}
+								}
+							}).create();
+		}
+		return null;
+	}
+}
